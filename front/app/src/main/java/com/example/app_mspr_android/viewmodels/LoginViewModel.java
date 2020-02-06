010100252849437ae09af90d131be5a094edf30c
laurence <l.sylva@epsi.fr>
2020-02-06 13:14:42 +0100

    intéraction entre les champs de la vue et le model

--- front/app/src/main/java/com/example/app_mspr_android/viewmodels/LoginViewModel.java
+++ front/app/src/main/java/com/example/app_mspr_android/viewmodels/LoginViewModel.java
@@ -1,5 +1,8 @@
 package com.example.app_mspr_android.viewmodels;
 
+import android.text.TextUtils;
+import android.util.Patterns;
+
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
 
@@ -13,6 +16,20 @@ import com.example.app_mspr_android.model.UserModel;
 public class LoginViewModel extends BaseObservable {
 
     private UserModel userModel;
+
+    @Bindable
+    private String toastMessage = null;
+    private String success = "you're in Mspr GoStyle";
+
+    @Bindable
+    public String getToastMessage() {
+        return toastMessage;
+    }
+
+    public void setToastMessage(String toastMessage) {
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(com.example.app_mspr_android.BR.toastMessage);
+    }
 
     public LoginViewModel(){
         userModel= new UserModel();
@@ -34,7 +51,15 @@ public class LoginViewModel extends BaseObservable {
     }
 
     public void setUserPassword(String passwd){
-        userModel.setEmail(passwd);
+        userModel.setPassword(passwd);
         notifyPropertyChanged(com.example.app_mspr_android.BR.userPassword);
+    }
+
+    public boolean isInputDataValid() {
+        return TextUtils.isEmpty(getUserEmail()) && Patterns.EMAIL_ADDRESS.matcher(getUserEmail()).matches() && getUserPassword().length() > 5;
+    }
+
+    public void onClicked() {
+        setToastMessage(success);
     }
 }
--- front/app/src/main/java/com/example/app_mspr_android/views/MainActivity.java
+++ front/app/src/main/java/com/example/app_mspr_android/views/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.app_mspr_android.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.app_mspr_android.R;
 import com.example.app_mspr_android.databinding.ActivityMainBinding;
@@ -18,5 +21,11 @@ public class MainActivity extends AppCompatActivity {
         activityMainBinding.setUserViewModel(new LoginViewModel());
         activityMainBinding.executePendingBindings();
 
+    }
+
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view, String message) {
+        if (message != null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
